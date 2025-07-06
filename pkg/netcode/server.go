537554a0e764cf0ce64e42b122e1c08b69867f3e
Ricky Palmer <ricky@rpgrafx.com>
2025-07-06 13:45:49 -0700

    Implement peer connection limit and tests

--- pkg/netcode/server.go
+++ pkg/netcode/server.go
@@ -1,9 +1,99 @@
 package netcode
 
+import (
+    "net"
+    "net/http"
+    "sync"
+
+    "github.com/gorilla/websocket"
+)
+
 type Server struct {
-	Address string
+    Address  string
+    upgrader websocket.Upgrader
+
+    listener net.Listener
+    srv      *http.Server
+    mu       sync.Mutex
+    conns    map[*websocket.Conn]struct{}
 }
 
 func NewServer(addr string) *Server {
-	return &Server{Address: addr}
+    s := &Server{
+        Address: addr,
+        upgrader: websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }},
+        conns:    make(map[*websocket.Conn]struct{}),
+    }
+    return s
+}
+
+// Start begins listening for websocket connections on the server's address.
+func (s *Server) Start() error {
+    ln, err := net.Listen("tcp", s.Address)
+    if err != nil {
+        return err
+    }
+    s.listener = ln
+    mux := http.NewServeMux()
+    mux.HandleFunc("/ws", s.handleWS)
+    srv := &http.Server{Handler: mux}
+    s.srv = srv
+    go srv.Serve(ln)
+    return nil
+}
+
+// Stop shuts down the server and closes all active connections.
+func (s *Server) Stop() error {
+    if s.srv != nil {
+        _ = s.srv.Close()
+    }
+    if s.listener != nil {
+        _ = s.listener.Close()
+    }
+    s.mu.Lock()
+    for c := range s.conns {
+        c.Close()
+    }
+    s.conns = make(map[*websocket.Conn]struct{})
+    s.mu.Unlock()
+    return nil
+}
+
+func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
+    s.mu.Lock()
+    if len(s.conns) >= 3 {
+        s.mu.Unlock()
+        http.Error(w, "connection limit reached", http.StatusServiceUnavailable)
+        return
+    }
+    s.mu.Unlock()
+
+    conn, err := s.upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        return
+    }
+
+    s.mu.Lock()
+    s.conns[conn] = struct{}{}
+    s.mu.Unlock()
+
+    go s.readLoop(conn)
+}
+
+func (s *Server) readLoop(c *websocket.Conn) {
+    defer func() {
+        s.mu.Lock()
+        delete(s.conns, c)
+        s.mu.Unlock()
+        c.Close()
+    }()
+    for {
+        mt, msg, err := c.ReadMessage()
+        if err != nil {
+            break
+        }
+        if err := c.WriteMessage(mt, msg); err != nil {
+            break
+        }
+    }
 }
